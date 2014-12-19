be2b25e0a5dbb911f3fec258733c0e59d98d948b
KangSanghun <myks789@gmail.com>
2014-12-19 13:15:25 +0900

    8층 엘레베이터 안에서 다른 순서로 층을 눌렀을때 테스트 코드 작성

--- src/test/java/process/ProcessJobTest.java
+++ src/test/java/process/ProcessJobTest.java
@@ -20,8 +20,30 @@ public class ProcessJobTest {
         ProcessJob processJob =new ProcessJob(inputBuffer,elevator);
         ArrayList result = new ArrayList();
 
+        elevator.setCurrentFloor(8);
+
         inputBuffer.selectFloorInElevator(1);
         ArrayList targetFloor = processJob.createTargetFloorList();
+        result.add(1);
+
+        assertThat(targetFloor, is(result));
+    }
+
+    @Test
+    public void testInElevatorSelect2_3_1()throws Exception{
+        InputBuffer inputBuffer = new InputBuffer();
+        Elevator elevator = new Elevator();
+        ProcessJob processJob =new ProcessJob(inputBuffer,elevator);
+        ArrayList result = new ArrayList();
+
+        elevator.setCurrentFloor(8);
+
+        inputBuffer.selectFloorInElevator(2);
+        inputBuffer.selectFloorInElevator(3);
+        inputBuffer.selectFloorInElevator(1);
+        ArrayList targetFloor = processJob.createTargetFloorList();
+        result.add(3);
+        result.add(2);
         result.add(1);
 
         assertThat(targetFloor, is(result));
